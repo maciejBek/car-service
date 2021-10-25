06f2bc1e4ff0e4273e5b346c5d5d4ba359c337f1
Maciej <maciek8928@o2.pl>
2021-10-25 12:27:01 +0200

    project files added

---
+++ backend/src/main/java/pl/company/carservice/CarserviceApplication.java
@@ -0,0 +1,13 @@
+package pl.company.carservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarserviceApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/pl/company/carservice/CarserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.company.carservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
