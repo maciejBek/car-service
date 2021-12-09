19c455d539b07643120528e6665392c5371d29c1
maciejBek <maciek8928@o2.pl>
2021-12-09 23:26:25 +0100

    Car entity modified

--- backend/src/main/java/pl/company/carservice/model/Car.java
+++ backend/src/main/java/pl/company/carservice/model/Car.java
@@ -1,15 +1,103 @@
 package pl.company.carservice.model;
 
-public record Car(
-        int id,
-        String brand,
-        String model,
-        int year,
-        String vinNumber,
-        String registrationNumber,
-        int capacity,
-        String fuelType,
-        int power,
-        int tourqe
-        ) {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+        private String brand;
+        private String model;
+        private int year;
+        private String vinNumber;
+        private String registrationNumber;
+        private int capacity;
+        private String fuelType;
+        private int power;
+        private int tourqe;
+
+        public long getId() {
+                return id;
+        }
+
+        public void setId(long id) {
+                this.id = id;
+        }
+
+        public String getBrand() {
+                return brand;
+        }
+
+        public void setBrand(String brand) {
+                this.brand = brand;
+        }
+
+        public String getModel() {
+                return model;
+        }
+
+        public void setModel(String model) {
+                this.model = model;
+        }
+
+        public int getYear() {
+                return year;
+        }
+
+        public void setYear(int year) {
+                this.year = year;
+        }
+
+        public String getVinNumber() {
+                return vinNumber;
+        }
+
+        public void setVinNumber(String vinNumber) {
+                this.vinNumber = vinNumber;
+        }
+
+        public String getRegistrationNumber() {
+                return registrationNumber;
+        }
+
+        public void setRegistrationNumber(String registrationNumber) {
+                this.registrationNumber = registrationNumber;
+        }
+
+        public int getCapacity() {
+                return capacity;
+        }
+
+        public void setCapacity(int capacity) {
+                this.capacity = capacity;
+        }
+
+        public String getFuelType() {
+                return fuelType;
+        }
+
+        public void setFuelType(String fuelType) {
+                this.fuelType = fuelType;
+        }
+
+        public int getPower() {
+                return power;
+        }
+
+        public void setPower(int power) {
+                this.power = power;
+        }
+
+        public int getTourqe() {
+                return tourqe;
+        }
+
+        public void setTourqe(int tourqe) {
+                this.tourqe = tourqe;
+        }
 }
