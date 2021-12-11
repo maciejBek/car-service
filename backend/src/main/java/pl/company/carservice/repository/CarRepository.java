b8ee48699fa7deaed78607baa401833870ecf1b8
maciejBek <maciek8928@o2.pl>
2021-12-11 16:33:50 +0100

    add functions

--- backend/src/main/java/pl/company/carservice/repository/CarRepository.java
+++ backend/src/main/java/pl/company/carservice/repository/CarRepository.java
@@ -5,4 +5,11 @@ import pl.company.carservice.model.Car;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
 
+    Long removeById(Long id);
+
+    boolean existsById(Long id);
+
+//    Long removeByBrand(String brand);
+//    List<Car> deleteByBrand(String bradn);
+
 }
