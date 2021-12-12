fd850671815f18ad36ccec3d4b337eb506e021a8
maciejBek <maciek8928@o2.pl>
2021-12-12 13:08:05 +0100

    add entity

---
+++ backend/src/main/java/pl/company/carservice/model/Feedback.java
@@ -0,0 +1,35 @@
+package pl.company.carservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+
+    public Feedback() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
