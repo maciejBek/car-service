a111be9c2e94ac9fe55d417cc81215ffdff8f6df
maciejBek <maciek8928@o2.pl>
2021-12-13 09:07:54 +0100

    add entity

---
+++ backend/src/main/java/pl/company/carservice/model/Order.java
@@ -0,0 +1,54 @@
+package pl.company.carservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date orderDate;
+    private Date Date;
+    private Double totalPrice;
+
+    public Order() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public java.sql.Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(java.sql.Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public java.sql.Date getDate() {
+        return Date;
+    }
+
+    public void setDate(java.sql.Date date) {
+        Date = date;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
