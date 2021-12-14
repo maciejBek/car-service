3a4cc4603b3ad8b6788ba220e0f5cae954e7131c
maciejBek <maciek8928@o2.pl>
2021-12-14 21:22:57 +0100

    add 409 status response

--- backend/src/main/java/pl/company/carservice/controller/AddressController.java
+++ backend/src/main/java/pl/company/carservice/controller/AddressController.java
@@ -1,7 +1,21 @@
 package pl.company.carservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.company.carservice.StringToJson;
+import pl.company.carservice.model.Address;
+import pl.company.carservice.model.Car;
+import pl.company.carservice.service.CarService;
 
+import java.util.Map;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
+@RequestMapping("/api")
 public class AddressController {
+
 }
