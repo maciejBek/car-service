7053126cf8017360440f022ef45f7aabb55ce971
Filip50n <nic990000@gmail.com>
2021-12-14 20:09:03 +0100

    dalsze zmiany dotyczace fornt

--- frontend/src/components/Rejestracja.js
+++ frontend/src/components/Rejestracja.js
@@ -1,7 +1,69 @@
 import React from 'react';
 import './Rejestracja.css';
+import axios from "axios";
+
+const CAR_REST_API_URL = 'http://localhost:8080/api/accounts';
 
 class Rejestracja extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isGoing: true,
+            numberOfGuests: 2
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        // getting data from form and putting to json string to body array
+        let account = document.getElementById('account');
+        let formData = new FormData(account);
+        let haslo1 = document.getElementById('haslo1').innerHTML
+
+        var data = {};
+        formData.forEach(function(value, key){
+            data[key] = value;
+        });
+
+        console.log(haslo1)
+
+
+        let body = JSON.stringify(data);
+
+        // add car to database with post method
+        axios({
+            method: "post",
+            url: CAR_REST_API_URL,
+            data: body,
+            headers: { "Content-Type": "application/JSON" },
+        })
+            .then(function (response) {
+                //handle success
+                console.log(response);
+            })
+            .catch(function (response) {
+                //handle error
+                console.log(response);
+            });
+
+        event.preventDefault();
+    }
+
+
+
     render() {
         return (
             //<div id="strona">
@@ -12,27 +74,29 @@ class Rejestracja extends React.Component {
                 <div className="text">
                     Rejestracja
                 </div>
+                <form id="account" onSubmit={this.handleSubmit}>
                 <div id="poz">
                     <div className="pasek">
                         <div id="zdj3"></div>
-                        <input className="input" type="text" name="nazwa" placeholder="E-mail"/>
+                        <input className="input" type="text" name="emailAddress" placeholder="E-mail" value={this.state.value} onChange={this.handleChange}/>
                     </div>
                     <div className="pasek">
                         <div id="zdj1"></div>
-                        <input className="input" type="text" name="nazwa" placeholder="Login"/>
+                        <input className="input" type="text" name="username" placeholder="Login" value={this.state.value} onChange={this.handleChange}/>
                     </div>
                     <div className="pasek">
                         <div id="zdj2"></div>
-                        <input className="input" type="text" name="nazwa" placeholder="Hasło"/>
+                        <input className="input" id="haslo1" type="password" name="password" placeholder="Hasło" value={this.state.value} onChange={this.handleChange}/>
                     </div>
                     <div className="pasek">
                         <div id="zdj4"></div>
-                        <input className="input" type="text" name="nazwa" placeholder="Powtórz Hasło"/>
+                        <input className="input" id="haslo2" type="password" name="password" placeholder="Hasło" value={this.state.value} onChange={this.handleChange}/>
                     </div>
                 </div>
                 <div id="przycisk">
                     <input id="przycisk1" type="submit" value="Zarejestruj się" />
                 </div>
+                </form>
             </div>
             //</div>
         );
