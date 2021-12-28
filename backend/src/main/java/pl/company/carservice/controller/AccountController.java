953db25c208dc7cd00164443146cf419c6eeab4f
maciejBek <maciek8928@o2.pl>
2021-12-28 12:39:27 +0100

    correct class

--- backend/src/main/java/pl/company/carservice/controller/AccountController.java
+++ backend/src/main/java/pl/company/carservice/controller/AccountController.java
@@ -1,16 +1,11 @@
 package pl.company.carservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pl.company.carservice.controller.error.ErrorResponse;
+import pl.company.carservice.dto.AccountRegistrationDto;
 import pl.company.carservice.model.Account;
 import pl.company.carservice.service.AccountService;
-
-import java.util.Map;
-import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -25,47 +20,24 @@ public class AccountController {
     }
 
     // TODO: add DTO to get data about account without password
-    @GetMapping(value = "/accounts/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/accounts/{id}", produces = "application/json")
     public ResponseEntity<?> getAccount(@PathVariable Long id) {
-        Optional<Account> fetchedAccount = accountService.getAccount(id);
-
-        if (fetchedAccount.isPresent()) {
-            Account account = fetchedAccount.get();
-            return new ResponseEntity<>(account, HttpStatus.OK);
-        } else {
-            ErrorResponse errorResponse = new ErrorResponse("Account with specified id does not exist!");
-            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @PostMapping("/accounts")
-    public ResponseEntity<?> addAccount(@RequestBody Account account) {
-        if (accountService.ifExistsByUsername(account.getUsername())) {
-            if (accountService.ifExistsByEmailAddress(account.getEmailAddress())) {
-                ErrorResponse errorResponse = new ErrorResponse("emailAddress and username exists");
-                return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
-            }
-            ErrorResponse errorResponse = new ErrorResponse("username exists");
-            return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
-        }
-
-        if (accountService.ifExistsByEmailAddress(account.getEmailAddress())) {
-            ErrorResponse errorResponse = new ErrorResponse("emailAddress exists");
-            return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
-//            return new ResponseEntity<>(Map.of("error", "emailAddress exists"), HttpStatus.CONFLICT);
-        }
-        // TODO: make DTO
-        return new ResponseEntity<>(Map.of("id", accountService.addAccount(account)), HttpStatus.OK);
+        return accountService.getAccount(id);
     }
 
     @DeleteMapping("/accounts/{id}")
     public ResponseEntity<?> deleteAccount(@PathVariable Long id) {
-        if (accountService.ifExistsById(id)) {
-            accountService.deleteAccount(id);
-            return ResponseEntity.status(HttpStatus.OK).body("");
-        } else {
-            ErrorResponse errorResponse = new ErrorResponse("Account with specified id does not exist!");
-            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-        }
+        return this.accountService.deleteAccount(id);
     }
+
+    @PostMapping("/accounts/login")
+    public ResponseEntity<?> login(@RequestBody AccountRegistrationDto accountRegistrationDto) {
+        return this.accountService.login(accountRegistrationDto);
+    }
+
+    @PostMapping("/accounts/register")
+    public ResponseEntity<?> register(@RequestBody Account account) {
+        return this.accountService.register(account);
+    }
+
 }
