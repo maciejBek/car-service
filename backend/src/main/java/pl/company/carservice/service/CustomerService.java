7c634833ecacebe39f50abb9bbbe75cb2c640f81
maciejBek <maciek8928@o2.pl>
2021-12-28 12:50:08 +0100

    add another service functionallity

--- backend/src/main/java/pl/company/carservice/service/CustomerService.java
+++ backend/src/main/java/pl/company/carservice/service/CustomerService.java
@@ -1,7 +1,47 @@
 package pl.company.carservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import pl.company.carservice.StringToJson;
+import pl.company.carservice.controller.error.ErrorResponse;
+import pl.company.carservice.model.Customer;
+import pl.company.carservice.repository.CustomerRepository;
+
+import java.util.Optional;
 
 @Service
 public class CustomerService {
+
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public ResponseEntity<?> getCustomer(Long id) {
+        Optional<Customer> fetchedCustomer = this.customerRepository.findById(id);
+
+        if (fetchedCustomer.isPresent()) {
+            return new ResponseEntity<>(fetchedCustomer.get(), HttpStatus.OK);
+        } else {
+            ErrorResponse errorResponse = new ErrorResponse("customer-does-not-exist");
+            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public Long addCustomer(Customer customer) {
+        return this.customerRepository.save(customer).getId();
+    }
+
+    public void deleteCar(Long id) {
+        this.customerRepository.deleteById(id);
+    }
+
+    public boolean ifExists(Long id) {
+        return this.customerRepository.existsById(id);
+    }
+
 }
