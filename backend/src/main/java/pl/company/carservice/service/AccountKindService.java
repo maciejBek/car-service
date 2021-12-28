86088d7f81cb52cb587c299fb63ae8953b373033
maciejBek <maciek8928@o2.pl>
2021-12-28 13:02:23 +0100

    add functionallities

--- backend/src/main/java/pl/company/carservice/service/AccountKindService.java
+++ backend/src/main/java/pl/company/carservice/service/AccountKindService.java
@@ -1,7 +1,28 @@
 package pl.company.carservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import pl.company.carservice.model.AccountKind;
+import pl.company.carservice.repository.AccountKindRepository;
+import pl.company.carservice.model.AccountKind.PermissionLevel;
 
 @Service
 public class AccountKindService {
+
+    private AccountKindRepository accountKindRepository;
+
+    @Autowired
+    public AccountKindService(AccountKindRepository accountKindRepository) {
+        this.accountKindRepository = accountKindRepository;
+    }
+
+    @EventListener(classes = ApplicationStartedEvent.class )
+    public void listenToStart(ApplicationStartedEvent event) {
+        this.accountKindRepository.save(new AccountKind(PermissionLevel.ADMIN));
+        this.accountKindRepository.save(new AccountKind(PermissionLevel.CUSTOMER));
+        this.accountKindRepository.save(new AccountKind(PermissionLevel.EMPLOYEE));
+        this.accountKindRepository.save(new AccountKind(PermissionLevel.SUPPLIER));
+    }
 }
