b4d6fe1fe300d5a6d152c38e21af4b0ccbea5d84
maciejBek <maciek8928@o2.pl>
2021-12-28 13:00:36 +0100

    add relationships

--- backend/src/main/java/pl/company/carservice/model/Car.java
+++ backend/src/main/java/pl/company/carservice/model/Car.java
@@ -1,19 +1,22 @@
 package pl.company.carservice.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Car {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(updatable = false)
         private Long id;
+
+        @ManyToOne
+        private Customer customer;
         private String brand;
         private String model;
         private Integer year;
+
+        @Column(unique = true)
         private String vinNumber;
         private String registrationNumber;
         private Integer capacity;
@@ -31,6 +34,14 @@ public class Car {
 
         public void setId(Long id) {
                 this.id = id;
+        }
+
+        public Customer getCustomer() {
+                return customer;
+        }
+
+        public void setCustomer(Customer customer) {
+                this.customer = customer;
         }
 
         public String getBrand() {
