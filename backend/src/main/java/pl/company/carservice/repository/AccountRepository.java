01fc4decc6dacc82d92743aa7a5a6e6949b325a7
maciejBek <maciek8928@o2.pl>
2021-12-28 13:01:36 +0100

    add functions

--- backend/src/main/java/pl/company/carservice/repository/AccountRepository.java
+++ backend/src/main/java/pl/company/carservice/repository/AccountRepository.java
@@ -4,4 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.company.carservice.model.Account;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    boolean existsByEmailAddress(String emailAddress);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByPassword(String password);
+
+    boolean existsByUsernameIsContainingAndPassword(String username, String password);
 }
