e803a4a7189e38eccb5eefc54165bdf3a91665a7
Filip50n <nic990000@gmail.com>
2021-12-29 18:00:38 +0100

    rejestracja gotowa

--- frontend/src/components/Rejestracja.js
+++ frontend/src/components/Rejestracja.js
@@ -39,41 +39,85 @@ class Rejestracja extends React.Component {
         });
 
         console.log(password1)
+        document.getElementById('pasek0').style.border = "0px solid red";
+        document.getElementById('pasek1').style.border = "0px solid red";
+        document.getElementById('pasek2').style.border = "0px solid red";
+        document.getElementById('pasek3').style.border = "0px solid red";
+        document.getElementById('blad').style.display = "none";
+        document.getElementById('blad1').style.display = "none";
+        document.getElementById('blad2').style.display = "none";
+        document.getElementById('blad3').style.display = "none";
+        document.getElementById('blad4').style.display = "none";
 
+        let email =document.getElementById('email').value
+        let login =document.getElementById('login').value
         let haslo1 =document.getElementById('haslo1').value
         let haslo2 =document.getElementById('haslo2').value
+
+        if(email == ""){
+            document.getElementById('pasek0').style.border="3px solid red";
+            document.getElementById('blad4').style.display = "inline";
+        }
+        if(login == ""){
+            document.getElementById('pasek1').style.border="3px solid red";
+            document.getElementById('blad4').style.display = "inline";
+        }
+        if(haslo1 == ""){
+            document.getElementById('pasek2').style.border="3px solid red";
+            document.getElementById('blad4').style.display = "inline";
+        }
+        if(haslo2 == ""){
+            document.getElementById('pasek3').style.border="3px solid red";
+            document.getElementById('blad4').style.display = "inline";
+        }
+
+
+        if(email != "" && login !="" && haslo1 !="" && haslo2 != ""){
         if(haslo1 != haslo2){
             console.log("hasla sie roznia")
-            document.getElementById('pasek1').style.border="3px solid red";
             document.getElementById('pasek2').style.border="3px solid red";
+            document.getElementById('pasek3').style.border="3px solid red";
             document.getElementById('blad').style.display="inline";
 
         }else {
             console.log("hasla zgodne")
-            document.getElementById('pasek1').style.border = "0px solid red";
-            document.getElementById('pasek2').style.border = "0px solid red";
-            document.getElementById('blad').style.display = "none";
-        }
 
-        let body = JSON.stringify(data);
 
-        // add car to database with post method
-        axios({
-            method: "post",
-            url: CAR_REST_API_URL,
-            data: body,
-            headers: { "Content-Type": "application/JSON" },
-        })
 
-            .then(function (response) {
-                //handle success
+            let body = JSON.stringify(data);
+
+            // add car to database with post method
+            axios({
+                method: "post",
+                url: CAR_REST_API_URL,
+                data: body,
+                headers: {"Content-Type": "application/JSON"},
             })
-            .catch(function (response) {
-                console.log(response.response.data);
-            }) .catch(function (response) {
-            //handle error
-            console.log(response);
-        });
+
+                .then(function (response) {
+                    document.getElementById('dobrze').style.display = "inline";
+                })
+                .catch(function (response) {
+                    const error =(response.response.data);
+                    console.log(error.error)
+                    if(error.error == "email-address-and-username-exist"){
+                        document.getElementById('pasek0').style.border="3px solid red";
+                        document.getElementById('pasek1').style.border="3px solid red";
+                        document.getElementById('blad1').style.display = "inline";
+                    }else if(error.error == "email-address-exists"){
+                        document.getElementById('pasek0').style.border="3px solid red";
+                        document.getElementById('blad2').style.display = "inline";
+                    }else if(error.error == "username-exists"){
+                        document.getElementById('pasek1').style.border="3px solid red";
+                        document.getElementById('blad3').style.display = "inline";
+                    }
+
+                }).catch(function (response) {
+                //handle error
+                console.log(response);
+            });
+        }
+        }
 
 
         event.preventDefault();
@@ -93,26 +137,42 @@ class Rejestracja extends React.Component {
                 </div>
                 <form id="account" onSubmit={this.handleSubmit}>
                     <div id="poz">
-                        <div className="pasek">
+                        <div className="pasek" id="pasek0">
                             <div id="zdj3"></div>
-                            <input className="input" type="text" name="emailAddress" placeholder="E-mail" value={this.state.value} onChange={this.handleChange}/>
-                        </div>
-                        <div className="pasek">
-                            <div id="zdj1"></div>
-                            <input className="input" type="text" name="username" placeholder="Login" value={this.state.value} onChange={this.handleChange}/>
+                            <input className="input" id="email" type="text" name="emailAddress" placeholder="E-mail" value={this.state.value} onChange={this.handleChange}/>
                         </div>
                         <div className="pasek" id="pasek1">
+                            <div id="zdj1"></div>
+                            <input className="input" id="login" type="text" name="username" placeholder="Login" value={this.state.value} onChange={this.handleChange}/>
+                        </div>
+                        <div className="pasek" id="pasek2">
                             <div id="zdj2"></div>
                             <input className="input" id="haslo1" type="password" name="password" placeholder="Hasło" value={this.state.value} onChange={this.handleChange}/>
                         </div>
-                        <div className="pasek" id="pasek2">
+                        <div className="pasek" id="pasek3">
                             <div id="zdj4"></div>
                             <input className="input" id="haslo2" type="password" name="password" placeholder="Hasło" value={this.state.value} onChange={this.handleChange}/>
                         </div>
                     </div>
-                    <div id="blad">
+                    <div className="blad" id="blad">
                         podane hasła się różnią!
                     </div>
+                    <div className="blad" id="blad1">
+                        Podany e-mail oraz login są już wykorzystywane!
+                    </div>
+                    <div className="blad" id="blad2">
+                        Podany e-mail jest już wykorzystany!
+                    </div>
+                    <div className="blad" id="blad3">
+                        Podany login jest już wykorzystany!
+                    </div>
+                    <div className="blad" id="blad4">
+                        Wypełnij zaznaczone pola!
+                    </div>
+                    <div id="dobrze">
+                        Konto zostało utworzone!
+                    </div>
+
                     <div id="przycisk">
                         <input id="przycisk1" type="submit" value="Zarejestruj się" />
                     </div>
