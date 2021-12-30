5993fffab877f1987b550af804817c2d492e55a0
Filip50n <nic990000@gmail.com>
2021-12-30 16:12:41 +0100

    Logowanie łaczenie z serwerem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 //import Form from './components/Form'
-//import Logowanie from './components/Logowanie';
-import Rejestracja from './components/Rejestracja';
+import Logowanie from './components/Logowanie';
+//import Rejestracja from './components/Rejestracja';
 //import Pracownik from './components/Pracownik';
 
 function App() {
   return (
-      //<Logowanie />
+      <Logowanie />
       //<Form/>
-      <Rejestracja/>
+      //<Rejestracja/>
       // <Pracownik/>
   );
 }
