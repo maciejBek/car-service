88584b7ef907e09622c295b2d2b4bfb2f9285d4d
Filip50n <nic990000@gmail.com>
2021-12-31 11:52:49 +0100

    pracownik menu urzytkownika

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 //import Form from './components/Form'
-import Logowanie from './components/Logowanie';
+//import Logowanie from './components/Logowanie';
 //import Rejestracja from './components/Rejestracja';
-//import Pracownik from './components/Pracownik';
+import Pracownik from './components/Pracownik';
 
 function App() {
   return (
-      <Logowanie />
+      //<Logowanie />
       //<Form/>
       //<Rejestracja/>
-      // <Pracownik/>
+       <Pracownik/>
   );
 }
 
