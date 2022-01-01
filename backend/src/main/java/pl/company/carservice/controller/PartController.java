9a9e115832ee70b910be4a354286cb4471479064
maciejBek <maciek8928@o2.pl>
2022-01-01 23:22:23 +0100

    add api

---
+++ backend/src/main/java/pl/company/carservice/controller/PartController.java
@@ -0,0 +1,35 @@
+package pl.company.carservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.company.carservice.dto.CarCustomerIdDto;
+import pl.company.carservice.model.Part;
+import pl.company.carservice.service.CarService;
+import pl.company.carservice.service.PartService;
+
+@RequestMapping("/api")
+public class PartController {
+
+    private PartService partService;
+
+    @Autowired
+    public PartController(PartService partService) {
+        this.partService = partService;
+    }
+
+    @GetMapping(value = "/parts/{id}", produces = "application/json")
+    public ResponseEntity<?> getPart(@PathVariable Long id) {
+        return this.partService.getPart(id);
+    }
+
+    @PostMapping("/parts")
+    public ResponseEntity<?> addPart(@RequestBody Part part) {
+        return this.partService.addPart(part);
+    }
+
+    @DeleteMapping("/parts/{id}")
+    public ResponseEntity<?> deletePart(@PathVariable Long id) {
+        return this.partService.deletePart(id);
+    }
+}
