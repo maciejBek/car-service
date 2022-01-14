421823b7e23884237575a5af2e0c2f468b96ed96
maciejBek <maciek8928@o2.pl>
2022-01-14 14:02:20 +0100

    add class

---
+++ backend/src/main/java/pl/company/carservice/controller/NeededTaskPartsController.java
@@ -0,0 +1,39 @@
+package pl.company.carservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.company.carservice.dto.CarCustomerIdDto;
+import pl.company.carservice.dto.NeededTaskPartsDto;
+import pl.company.carservice.model.NeededTaskParts;
+import pl.company.carservice.service.CarService;
+import pl.company.carservice.service.NeededTaskPartsService;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api")
+public class NeededTaskPartsController {
+
+    private NeededTaskPartsService neededTaskPartsService;
+
+    @Autowired
+    public NeededTaskPartsController(NeededTaskPartsService neededTaskPartsService) {
+        this.neededTaskPartsService = neededTaskPartsService;
+    }
+
+    @GetMapping(value = "/needed-task-parts", produces = "application/json")
+    public ResponseEntity<?> getCars(
+            @RequestParam(defaultValue = "0") Integer pageNo,
+            @RequestParam(defaultValue = "100") Integer pageSize,
+            @RequestParam(defaultValue = "brand") String sortBy,
+            @RequestParam(defaultValue = "") Long customerId) {
+
+        return this.neededTaskPartsService.getNeededTaskParts(pageNo, pageSize, sortBy, customerId);
+    }
+
+    @PostMapping("/needed-task-parts")
+    public ResponseEntity<?> addNeededTaskParts(@RequestBody NeededTaskPartsDto neededTaskPartsDto) {
+        return this.neededTaskPartsService.addNeededTaskParts(neededTaskPartsDto);
+    }
+
+}
