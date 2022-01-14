82fcbc65e5d5f426e3832b0f7a7db34fddd0612d
maciejBek <maciek8928@o2.pl>
2022-01-14 15:11:29 +0100

    add adding vacation functionallity

--- backend/src/main/java/pl/company/carservice/controller/VacationController.java
+++ backend/src/main/java/pl/company/carservice/controller/VacationController.java
@@ -1,7 +1,25 @@
 package pl.company.carservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.company.carservice.dto.VacationDto;
+import pl.company.carservice.service.VacationService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
+@RequestMapping("/api")
 public class VacationController {
+
+    private VacationService vacationService;
+
+    @Autowired
+    public VacationController(VacationService vacationService) {
+        this.vacationService = vacationService;
+    }
+
+    @PostMapping(value = "/vacations", produces = "application/json")
+    public ResponseEntity<?> addVacation(@PathVariable VacationDto vacationDto) {
+        return vacationService.addVacation(vacationDto);
+    }
 }
--- backend/src/main/java/pl/company/carservice/service/VacationService.java
+++ backend/src/main/java/pl/company/carservice/service/VacationService.java
@@ -1,7 +1,58 @@
 package pl.company.carservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.company.carservice.StringToJson;
+import pl.company.carservice.dto.VacationDto;
+import pl.company.carservice.model.Account;
+import pl.company.carservice.model.Car;
+import pl.company.carservice.model.Employee;
+import pl.company.carservice.model.Vacation;
+import pl.company.carservice.repository.AccountRepository;
+import pl.company.carservice.repository.EmployeeRepository;
+import pl.company.carservice.repository.VacationRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class VacationService {
+
+    private VacationRepository vacationRepository;
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public VacationService(VacationRepository vacationRepository, EmployeeRepository employeeRepository) {
+        this.vacationRepository = vacationRepository;
+        this.employeeRepository = employeeRepository;
+    }
+
+    public ResponseEntity<?> addVacation(VacationDto vacationDto) {
+        LocalDate startDate = LocalDate.parse(vacationDto.startDate());
+        LocalDate endDate = LocalDate.parse(vacationDto.endDate());
+        Long accountId = vacationDto.accountId();
+
+        Optional<Employee> fetchedEmployee = this.employeeRepository.findById(accountId);
+
+        Employee employee = null;
+        if (fetchedEmployee.isPresent()) {
+            employee = fetchedEmployee.get();
+        } else {
+            String errorResponse = StringToJson.parse("error", "employee-does-not-exist");
+            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        }
+
+        Vacation vacation = new Vacation(employee, startDate, endDate);
+
+        // save to database
+        Long vacationId = vacationRepository.save(vacation).getId();
+
+        return new ResponseEntity<>(Map.of("id", vacationId), HttpStatus.OK);
+    }
+
 }
